cf3cba2dd42f2a82525410f7cb4b1c5862751081
KodaCamp <>
2022-11-29 21:46:31 +0800

    [Feature 3] Setup bootstrap - adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  #
   constraints(AdminDomainConstraint.new) do
-    namespace :admin do
+    namespace :admin, path: '' do
+      root to: "home#index"
+      devise_for :users, controllers: { sessions: 'admin/sessions' }
       resources :users
+      resources :home
     end
   end
 
   constraints(ClientDomainConstraint.new) do
-    namespace :client do
-      resources :users
+    devise_for :users, controllers: { sessions: 'clients/sessions', registrations: 'clients/registrations' }
+    namespace :clients, path: '' do
+      root to: "home#index"
+      resources :home
+      resources :profile
     end
   end
 end
