8694634a46d17fa989fe120cbb8b0d5b2582632a
KodaCamp <>
2022-11-30 17:27:25 +0800

    [Feature 2] Admin and Client Constraint - generate admin controller and modify it to actioncontroller::base - remove def after_sign_in for current admin - refactor admin::users controller to admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class AdminController < ActionController::Base
   def after_sign_up_path_for(resource)
     after_sign_in_path_for(resource) if is_navigational_format?
   end
