7c2dc1db861b8f7461efd1943b380f413c59f9de
KodaCamp <>
2022-11-30 17:09:54 +0800

    [Feature 4] User profile - add mount_uploader in user model - add include carrierwave::minimagick to resize image when upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :phone, phone: { possible: true, allow_blank: true, types: [:voip, :mobile], countries: :ph }
-
+  mount_uploader :image, ImageUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
