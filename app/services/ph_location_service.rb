b384b10ab44d2ced7272607eb98df5bf7b2a71d9
KodaCamp <>
2022-12-01 11:19:26 +0800

    [Feature 5] Get opendata from ph goverment 'Province' - create address::province model and migrate - validates name and code - add has_many to region model and belongs_to province model - done services for fetch_province

--- app/services/ph_location_service.rb
+++ app/services/ph_location_service.rb
@@ -6,12 +6,28 @@ class PhLocationService
   end
 
   def fetch_region
-    request = RestClient.get("#{url}/regions/")
+    request = RestClient.get("#{url}/regions")
     data = JSON.parse(request.body)
     data.each do |region|
       address_region = Address::Region.find_or_initialize_by(code: region['code'])
       address_region.name = region['regionName']
       address_region.save
+    end
+  end
+
+  def fetch_province
+    request = RestClient.get("#{url}/provinces")
+    provinces = JSON.parse(request.body)
+    provinces.each do |province|
+      region = Address::Region.find_by_code(province['regionCode'])
+      Address::Province.find_or_create_by(code: province['code'], name: province['name'], region: region)
+    end
+
+    request = RestClient.get("#{url}/districts")
+    districts = JSON.parse(request.body)
+    districts.each do |district|
+      region = Address::Region.find_by_code(district['regionCode'])
+      Address::Province.find_or_create_by(code: district['code'], name: district['name'], region: region)
     end
   end
 end
