a0fe4e45f68f735aa793f19c2117d82b7e2a1e25
KodaCamp <>
2022-12-01 10:50:57 +0800

    [Feature 5] Get opendata from ph goverment 'Region' - create address::region model and migrate - validates name and code - done services for fetch_region

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_082852) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_022650) do
+  create_table "address_regions", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
