a6db48b408f4ebe2e5345a32d2d610711819c4c9
KodaCamp <>
2022-12-06 20:05:37 +0800

    [Feature 11] Item Soft delete "Done soft kill" - git_ignore uploaded img

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,12 @@
 class Item < ApplicationRecord
   validates :name, :image, :quantity, :minimum_bets, :online_at,
             :offline_at, :start_at, :status, presence: true
+  default_scope { where(deleted_at: nil) }
   mount_uploader :image, ImageUploader
 
   enum status: { inactive: 0, active: 1 }
+
+  def destroy
+    update(deleted_at: Time.current)
+  end
 end
---
+++ db/migrate/20221206112920_add_column_deleted_at_items.rb
@@ -0,0 +1,5 @@
+class AddColumnDeletedAtItems < ActiveRecord::Migration[7.0]
+  def change
+    add_column :items, :deleted_at, :datetime
+  end
+end
