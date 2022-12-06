a677082c88b543309966fdcb638863a58de1bda8
KodaCamp <>
2022-12-06 16:55:32 +0800

    [Feature 9] Admin User List "Done user list view" - index action find only clients role - done index view

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,5 @@
 class Admin::UsersController < AdminController
-  before_action :check_admin
-
   def index
-    @users = User.all
-  end
-
-  def check_admin
-    raise ActionController::RoutingError.new('Not Found') unless current_user.admin?
+    @users = User.where(role: 0)
   end
 end
