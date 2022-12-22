b6584afad5129245b86fea9e1792c1ecae3521d0
KodaCamp <>
2022-12-22 23:52:41 +0800

    [Feature 34] Display banner and news_ticker

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def address_join(address)
     "#{address.street_address}, #{address.barangay.name}, #{address.city_municipality.name}, #{address.province.name}, #{address.region.name}"
   end
+
+  def banners
+    Banner.where('online_at <= ? AND offline_at > ?', Time.current, Time.current).active
+  end
+
+  def news_tickers
+    NewsTicker.active.limit(5)
+  end
 end
