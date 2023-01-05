cff19c821072dc21febea6dac79f0d2e6d310e1a
KodaCamp <>
2023-01-05 17:26:16 +0800

    [Feature 30] Add view for client - tagalog and english

--- config/application.rb
+++ config/application.rb
@@ -28,9 +28,11 @@ module App
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Hong Kong"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.i18n.default_locale = "en"
+    config.i18n.available_locales = [:en, 'tl']
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
